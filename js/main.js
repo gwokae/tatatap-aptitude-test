05c7d20857e2bfddc2a62653bc58f0daa24113a1
gwokae <gwokae@gmail.com>
2014-09-11 17:05:54 +0800

    loading animate + basic layer 2 layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-
+$(function() {
+  // loading modal
+  var animate = function($e, direction) {
+    $e.each(function() {
+      this.dataset.in && this.classList.remove(this.dataset.in);
+      this.dataset.out && this.classList.remove(this.dataset.out);
+      this.dataset[direction] && this.classList.add(this.dataset[direction]);
+    });
+    if (direction === 'out') {
+      setTimeout(function() {
+        $e.hide();
+      }, 1000);
+    }
+    return $e;
+  };
+  var $loading = animate($('.loading.animated, .loading-overlay.animated'), 'in');
+  var cnt = 0,
+    $img = $('#container img').load(function() {
+      if (++cnt >= $img.length) {
+        animate($loading, 'out');
+      }
+    });
+});
